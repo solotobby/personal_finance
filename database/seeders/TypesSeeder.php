1e5750fff53fa4e3657d30c7c9aa949c30ab78f3
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-08-01 21:58:40 +0100

    fhfglsdjhfgsd

--- database/seeders/TypesSeeder.php
+++ database/seeders/TypesSeeder.php
@@ -15,18 +15,22 @@ class TypesSeeder extends Seeder
     public function run()
     {
         $transactionCategories = [
-            ['id' => 1, 'name' => 'Groceries', 'description' => ''],
-            ['id' => 2, 'name' => 'Shopping', 'description' => ''],
-            ['id' => 3, 'name' => 'Transport', 'description' => ''],
-            ['id' => 4, 'name' => 'Travel', 'description' => ''],
-            ['id' => 5, 'name' => 'Wellness', 'description' => ''],
-            ['id' => 6, 'name' => 'Housing', 'description' => ''],
-            ['id' => 7, 'name' => 'Entertainment', 'description' => ''],
-            ['id' => 8, 'name' => 'Fmily & Friends', 'description' => ''],
-            ['id' => 9, 'name' => 'Dinning', 'description' => ''],
-            ['id' => 10, 'name' => 'Bills & Utilities', 'description' => ''],
-            ['id' => 11, 'name' => 'Savings & Investment', 'description' => ''],
-            ['id' => 12, 'name' => 'Miscellaneous', 'description'=> '']
+            ['id' => 1, 'name' => 'Freebyz', 'description' => ''],
+            ['id' => 2, 'name' => 'Payhankey', 'description' => ''],
+            ['id' => 3, 'name' => 'Salaries', 'description' => ''],
+            ['id' => 4, 'name' => 'Groceries', 'description' => ''],
+            ['id' => 5, 'name' => 'Shopping', 'description' => ''],
+            ['id' => 6, 'name' => 'Transport', 'description' => ''],
+            ['id' => 7, 'name' => 'Travel', 'description' => ''],
+            ['id' => 8, 'name' => 'Wellness', 'description' => ''],
+            ['id' => 9, 'name' => 'Housing', 'description' => ''],
+            ['id' => 10, 'name' => 'Entertainment', 'description' => ''],
+            ['id' => 11, 'name' => 'Family & Friends', 'description' => ''],
+            ['id' => 12, 'name' => 'Dinning', 'description' => ''],
+            ['id' => 13, 'name' => 'Bills & Utilities', 'description' => ''],
+            ['id' => 14, 'name' => 'Savings', 'description' => ''],
+            ['id' => 15, 'name' => 'Investment', 'description' => ''],
+            ['id' => 16, 'name' => 'Miscellaneous', 'description'=> '']
         ];
 
         foreach($transactionCategories as $transactions)
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,6 @@ class UserSeeder extends Seeder
     public function run()
     {
         $users = [
-            
             ['name' => 'Oluwatobi Admin', 'email' => 'solo@ad.com', 'password' => bcrypt('solomon001'), 'role' => 'admin'],
             ['name' => 'Oluwatobi Solomon', 'email' => 'solando@pf.com', 'password' => bcrypt('solomon001'), 'role' => 'user'],
             ['name' => 'Samuel Tobi', 'email' => 'tobi@pf.com', 'password' => bcrypt('solomon001'), 'role' => 'user']
